a56fd149c71a064ed4b6b61ded119794e19a348e
SleiDan <dan.shvedun@gmail.com>
2023-08-06 02:56:27 +0300

    Level 3 added

--- GameJamCycles/Assets/Scripts/MainMenu.cs
+++ GameJamCycles/Assets/Scripts/MainMenu.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartMenu : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
-    public void StartGame()
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
     }
 }
